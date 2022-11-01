fb63e8b021e2f1e3a062dab37b757bce8f8ac290
Developer ILD <dev.ild@th-luebeck.de>
2022-11-01 14:27:08 +0100

    initial commit

---
+++ lang/de/format_mooin.php
@@ -0,0 +1,39 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component mooin course format.
+ *
+ * @package   format_mooin
+ * @copyright 2022 ISy TH Lübeck <dev.ild@th-luebeck.de>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['addsections'] = 'Abschnitt hinzufügen';
+$string['currentsection'] = 'Dieser Abschnitt';
+$string['editsection'] = 'Abschnitt bearbeiten';
+$string['editsectionname'] = 'Abschnitt-Name bearbeiten';
+$string['deletesection'] = 'Abschnitt löschen';
+$string['news'] = 'News';
+$string['newsectionname'] = 'Neuer Name für Abschnitt {$a}';
+$string['sectionname'] = 'Abschnitt';
+$string['pluginname'] = 'mooin format';
+$string['section0name'] = 'Allgemein';
+$string['page-course-view-mooin'] = 'Any course main page in mooin format';
+$string['page-course-view-mooin-x'] = 'Any course page in mooin format';
+$string['hidefromothers'] = 'Abschnitt verbergen';
+$string['showfromothers'] = 'Abschnitt anzeigen';
+$string['privacy:metadata'] = 'Das mooin Kursformat-Plugin speichert keine persönlichen Daten.';
---
+++ lang/en/format_mooin.php
@@ -0,0 +1,39 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component mooin course format.
+ *
+ * @package   format_mooin
+ * @copyright 2022 ISy TH Lübeck <dev.ild@th-luebeck.de>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['addsections'] = 'Add topics';
+$string['currentsection'] = 'This topic';
+$string['editsection'] = 'Edit topic';
+$string['editsectionname'] = 'Edit topic name';
+$string['deletesection'] = 'Delete topic';
+$string['news'] = 'News';
+$string['newsectionname'] = 'New name for topic {$a}';
+$string['sectionname'] = 'Topic';
+$string['pluginname'] = 'mooin format';
+$string['section0name'] = 'General';
+$string['page-course-view-mooin'] = 'Any course main page in mooin format';
+$string['page-course-view-mooin-x'] = 'Any course page in mooin format';
+$string['hidefromothers'] = 'Hide topic';
+$string['showfromothers'] = 'Show topic';
+$string['privacy:metadata'] = 'The mooin format plugin does not store any personal data.';
