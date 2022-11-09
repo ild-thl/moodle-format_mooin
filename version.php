4a32939558f9ffa90ded14a21321653d538292c2
Developer ILD <dev.ild@th-luebeck.de>
2022-11-09 09:28:29 +0100

    Changed minimum required moodle version

--- version.php
+++ version.php
@@ -25,5 +25,5 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2022110200;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2021051100;        // Requires this Moodle version.
+$plugin->requires  = 2020061515;        // Requires this Moodle version.
 $plugin->component = 'format_mooin';    // Full name of the plugin (used for diagnostics).
