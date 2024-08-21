d8be68eb0a6dff1bd5ddb0e609ba777728ddaa49
Kreuterberg <mails@jonasreuter.com>
2024-08-21 13:52:27 +0200

    added support for ukranian language pack

---
+++ lang/uk/format_mooin4.php
@@ -0,0 +1,185 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component mooin4 course format.
+ *
+ * @package   format_mooin4
+ * @copyright 2022 ISy TH Lübeck <dev.ild@th-luebeck.de>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['addsections'] = 'Додати розділ';
+$string['currentsection'] = 'Поточний розділ';
+$string['editsection'] = 'Редагувати розділ';
+$string['editsectionname'] = 'Редагувати назву розділу';
+$string['deletesection'] = 'Видалити розділ';
+$string['news'] = 'Новини';
+$string['newsectionname'] = 'Нова назва розділу {$a}';
+$string['sectionname'] = 'Розділ';
+$string['pluginname'] = 'Формат курсу Mooin 4.0';
+$string['section0name'] = 'Загальні';
+$string['page-course-view-mooin4'] = 'Головна сторінка будь-якого курсу у форматі mooin 4.0';
+$string['page-course-view-mooin4-x'] = 'Сторінка будь-якого курсу у форматі mooin 4.0';
+$string['hidefrommothers'] = 'Сховати розділ';
+$string['showfromothers'] = 'Показати розділ';
+$string['privacy:metadata'] = 'Плагін формату курсу Mooin 4.0 не зберігає жодних персональних даних';
+$string['map_title'] = 'Карта учасників';
+$string['map_descr'] = 'Будь ласка, вкажіть ваше місце проживання у вашому профілі, щоб карта учасників відображалася якомога повніше';
+$string['badges'] = 'Мої бейджики курсів';
+$string['community'] = 'Спільнота';
+$string['forums'] = 'Форуми для обговорень';
+$string['my_forum'] = 'Форуми';
+$string['participants'] = 'Учасники';
+$string['certificates'] = 'Сертифікати мого курсу';
+$string['badges_certificates'] = 'Бейджі та сертифікати';
+$string['continue'] = 'Продовжити навчання на уроці {$a}';
+$string['continue_no_lesson'] = 'Продовжувати навчання';
+$string['progress'] = 'Прогрес курсу';
+$string['unenrol'] = 'Відрахуватися з курсу';
+$string['my_badges'] = 'Бейджі';
+$string['available_badges'] = 'Доступні бейджі';
+$string['awarded_badges'] = 'Нещодавно здобуті бейджі';
+$string['hide_tabs'] = 'Приховати вкладки MOOC';
+$string['highscore'] = 'Високий бал';
+$string['in_course'] = 'У цьому курсі';
+$string['in_mooin4'] = 'У всіх курсах на платформі';
+$string['lastday'] = 'За останні 24 години';
+$string['lastweek'] = 'За останні 7 днів';
+$string['discussion_link'] = 'ID для загального форуму обговорення';
+$string['my_threads'] = 'Мої повідомлення';
+$string['chapter_config'] = 'Розділ';
+$string['name'] = 'Ім'я';
+$string['enabled'] = 'Видимий';
+$string['lections'] = 'Кількість уроків';
+$string['configtext'] = 'Параметри розділу';
+$string['directory_link'] = 'Ідентифікатор директорії для зображень розділів';
+$string['course_badges'] = 'Бейджі курсу';
+$string['overview'] = 'Огляд бейджів';
+$string['profile_badges'] = 'Всі бейджі в моєму профілі';
+$string['badge_options'] = 'Налаштування бейджа';
+$string['owner'] = 'Власник';
+$string['no_badges_awarded'] = 'На цьому курсі ще не було присуджено жодного бейджа.';
+$string['badge_overview_description'] = 'З допомогою значків курсу або цифрових значків навчання ти можеш показати іншим учасникам на цій платформі та в Інтернеті, які успіхи в курсі ти вже досягнув. Більше інформації можна знайти на <a href=«https://de.wikipedia.org/wiki/Open_Badges» target=«_blank»>Wikipedia</a> und bei <a href=«http://openbadges.org» target=«_blank»>OpenBadges.org</a>';
+
+
+$string['all_forums'] = 'Всі форуми для обговорення';
+$string['show_all_forums'] = 'Показати всі форуми для обговорення';
+$string['course_groups'] = 'Групи курсу';
+$string['course_groups_descr'] = 'Тут ви можете побачити, які робочі групи є в цьому курсі, і вирішити, до якої з них ви хотіли б приєднатися:';
+$string['profile_city_descr'] = 'Будь ласка, вкажіть у своєму профілі місце проживання, щоб карта учасників відображалася якомога повніше.';
+$string['number_of_questions'] = 'Кількість усіх запитань вікторини Capira';
+$string['cert_descr'] = 'Вітаємо! Ви успішно виконали щонайменше {$a} відсотків самоперевірок, включених до курсу. Ви можете завантажити свій сертифікат про проходження курсу тут';
+$string['certificate'] = 'Сертифікат про відвідування';
+$string['cert_addtext'] = 'Lorem ipsum';
+$string['required_questions'] = 'Необхідна кількість правильних відповідей у %';
+$string['only_for_trainers'] = 'Тільки для адміністраторів та тренерів';
+$string['my_certificate'] = 'Сертифікати курсу';
+$string['certificate_overview_description'] = 'Вітаємо! Ви успішно пройшли самотестування, яке було включено до курсу. Ви можете завантажити сертифікат про проходження курсу тут»;
+$string['certificate_overview'] = 'Для цього курсу ще немає сертифікату!
+
+$string['start'] = 'Старт';
+$string['startlesson'] = 'Почати з уроку 1.1';
+$string['chapter'] = 'Розділ';
+$string['chapter_short'] = 'Розділ';
+$string['lesson'] = 'Урок';
+$string['lesson_short'] = 'Урок';
+$string['configlabel_forcetrackforums'] = 'Позначати непрочитані дописи примусово';
+$string['configdesc_forcetrackforums'] = 'Завжди автоматично активовувати глобальні налаштування Moodle: <a href=«{$a}/admin/search.php?query=trackreadposts»>{$a}/admin/search.php?query=trackreadposts</a><br>
+										  Необхідна для того, щоб користувачі могли бачити, які повідомлення на форумі ще не прочитані. Непрочитані повідомлення можуть бути позначені як прочитані одним кліком. Це корисно, якщо користувачі реєструються на курс в той час, коли вже існує велика кількість повідомлень.';
+$string['update_config'] = 'Оновлення загальних налаштувань Moodle';
+$string['hint_track_forums'] = 'Будь ласка, оновіть налаштування форуму <a href=«{$a->wwwroot}/user/forum.php?id={$a->userid}&course=1»>hier</a>, щоб нові повідомлення позначалися кольором (відстеження форуму > Так, позначати нові повідомлення кольором)';
+$string['mark_all_as_read'] = 'Позначити все як прочитане';
+$string['mark_all_as_read_mobile'] = 'Читати повністю';
+$string['configlabel_geonamesapi_url'] = 'GeoNames API-URL';
+$string['configdesc_geonamesapi_url'] = 'Цей сервіс використовується для відображення координат учасників на карті учасників.';
+$string['configlabel_geonamesapi_username'] = 'Ім'я користувача GeoNames';
+
+$string['configdesc_geonamesapi_username'] = 'Для використання цього сервісу потрібен обліковий запис GeoNames. Його можна створити <a href=«https://www.geonames.org/»>hier</a> та активувати. Наш Mooin 4.0 демо-рахунок слід використовувати лише для тестування.';
+$string['configlabel_forcecompletiondefault'] = 'Не відображати завершення операції за замовчуванням';
+$string['configdesc_forcecompletiondefault'] = 'Коли до курсу додаються нові вправи, відстеження виконання за замовчуванням не відображається. Якщо для вправи ввімкнено відстеження завершення роботи, індикатор прогресу в цьому уроці залежить від того, чи вона завершена. В іншому випадку в нижньому колонтитулі (футері) уроку відображається кнопка, за допомогою якої урок можна позначити як завершений.;
+
+$string['unread_news'] = 'непрочитаних повідомлень у розділі';
+$string['all_news_mobile'] = 'Всі внески';
+$string['by'] = 'від';
+$string['latest_contribution_mobile'] = 'Останнє повідомлення:';
+$string['show_all_infos'] = 'Показати всю інформацію';
+$string['all_news'] = 'Форум новин';
+$string['topicoutline'] = 'Зміст курсу';
+$string['participant_map'] = 'Карта учасників';
+$string['no_badges_image_text'] = 'Немає бейджів';
+$string['no_certificates_image_text'] = 'Сертифікатів немає';
+$string['no_forums_available'] = 'Немає доступних форумів';
+$string['no_contributions_available'] = 'Внески ще не доступні';
+
+$string['modal_course_complete'] = 'Ви пройшли всі уроки цього курсу!
+$string['modal_course_complete_title'] = 'Курс завершено';
+$string['modal_chapter_complete'] = 'Ви пройшли всі уроки цього розділу';
+$string['modal_chapter_complete_title'] = 'Розділ завершено';
+$string['modal_last_section_of_chapter'] = 'Ви дійшли до останнього уроку цього розділу!
+$string['modal_last_section_of_chapter_title'] = 'Досягнуто кінця розділу';
+$string['close'] = 'Закрити';
+
+// Chapter in course, abgeglichen mit Englisch
+$string['new_chapter'] = 'Новий розділ';
+$string['new_lesson'] = 'Новий урок';
+$string['setchapter'] = 'Встановити як назву розділу';
+$string['unsetchapter'] = 'Встановити як урок';
+$string['completed'] = 'Готово';
+$string['headerimagedesktop'] = 'Зображення заголовка на робочому столі (800 x 280px)';
+$string['headerimagemobile'] = 'Зображення заголовка мобільний (768 x 768px)';
+$string['edit_course_header'] = 'Редагувати зображення заголовка курсу';
+$string['cannot_remove_chapter'] = 'Цей розділ не може бути видалений, оскільки не може бути уроків без основного розділу';
+$string['course_overview'] = 'Огляд курсу';
+
+// added by Jonas, war tw. nicht in Deutsch vorhanden
+$string['unread_news_single'] = 'непрочитані повідомлення в рубриці ';
+$string['unread_discussions'] = 'непрочитані повідомлення в ';
+$string['unread_discussions_single'] = 'непрочитані повідомлення в ';
+$string['discussion_forum'] = 'форуми обговорень';
+$string['next_lesson'] = 'наступний урок';
+$string['previous_lesson'] = 'Попередній урок';
+$string['next_chapter'] = 'Наступний розділ';
+$string['previous_chapter'] = 'Попередній розділ';
+$string['discussions_tooltip'] = 'В уроках створюються форуми для обговорення';
+$string['certificates_tooltip'] = 'В уроках створюються сертифікати';
+$string['newsforum_settings'] = 'Налаштування форуму новин';
+$string['course_progress_text'] = 'прогрес курсу';
+$string['lesson_progress_text'] = 'урок опрацьовано';
+$string['progress_text_short'] = 'опрацьовано';
+$string['mark_page_as_read'] = 'позначити сторінку як прочитану';
+$string['page_read'] = 'Сторінка прочитана';
+$string['allparticipants'] = 'Всі учасники';
+
+// Neu aus Englisch hinzugefügt
+$string['show_all_infos'] = ' Показати всю інформацію';
+$string['users'] = 'Учасники';
+$string['user_card_title'] = 'Учасники курсу';
+$string['user'] = 'Люди';
+$string['in_course'] = 'беруть участь';
+$string['new_user'] = 'Нещодавно доданий: ';
+$string['no_user'] = 'Учасників не додано!
+$string['welcome'] = 'Ласкаво просимо на курс';
+$string['forum_unread'] = 'Непрочитані форуми';
+$string['discussions'] = 'Всі обговорення';
+
+$string['discussion']= 'Останнє повідомлення';
+$string['all_discussions'] = 'Непрочитані повідомлення у форумах обговорення';
+$string['old_discussion'] = 'Старіші обговорення';
+$string['old_news'] = 'Старіші новини';
+$string['all_news'] = 'Форум новин';
+$string['latest_post'] = 'Останнє повідомлення';
+$string['discussion_news'] = 'До новин';
