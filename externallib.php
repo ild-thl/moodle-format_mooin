41e50fced44526073eef9aaa95077d1feeac02a2
tinjohn <tina.john@th-luebeck.de>
2024-10-09 16:33:05 +0200

    fix: external API not found

--- externallib.php
+++ externallib.php
@@ -1,9 +1,17 @@
 <?php
 
-require_once($CFG->libdir . "/externallib.php");
 require_once($CFG->dirroot . '/course/lib.php');
+//require_once($CFG->dirroot . '/lib/external/externallib.php');
 
 use format_mooin4\local\utils as utils;
+use core_external\external_api;
+use core_external\external_function_parameters;
+use core_external\external_single_structure;
+use core_external\external_multiple_structure;
+use core_external\external_format_value;
+use core_external\external_value;
+use core_external\external_warnings;
+
 
 class format_mooin4_external extends external_api
 {
