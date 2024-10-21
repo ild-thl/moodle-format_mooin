136d9f969d08d5e68eeb4150709295d0d42e4b42
tinjohn <tina.john@th-luebeck.de>
2024-10-21 15:48:25 +0200

    from 401 branch because of conflict

--- amd/src/ildhvp4.js
+++ amd/src/ildhvp4.js
@@ -1,340 +1,247 @@
-define([
-  "jquery",
-  "core/ajax",
-  "core/notification",
-  "core/str",
-  "core/url",
-  "core/modal_factory",
-], function (
-  $,
-  ajax,
-  notification,
-  Str,
-  Url,
-  ModalFactory,
-) {
-  /** @namespace */
-  var ILD = ILD || {};
+// Import der Abhängigkeiten
+import $ from 'jquery';
+import ajax from 'core/ajax';
+import notification from 'core/notification';
+import Str from 'core/str';
+import Url from 'core/url';
+import ModalFactory from 'core/modal_factory';
 
-  /**
-   * Interactions counter
-   * @type {Array}
-   */
-  ILD.interactions = [];
+// ILD Namespace Definition
+const ILD = {};
 
-  /**
-   * SingelChoiceInteractions counter
-   * @type {Array}
-   */
-  ILD.singleChoiceInteractions = [];
+// Interactions counter
+ILD.interactions = [];
 
-  /**
-   * SubContentIds - avoid duplicated answered statement
-   * @type {Array}
-   */
-  ILD.subIds = [];
+// SingleChoiceInteractions counter
+ILD.singleChoiceInteractions = [];
 
-  /**
-   * Stores QuestionSet PassPercentage
-   * @type {Array}
-   */
-  ILD.questionSetPassPercentage = [];
+// SubContentIds - avoid duplicated answered statement
+ILD.subIds = [];
 
-  /**
-   * Stores Essay PassPercentage
-   * @type {Array}
-   */
-  ILD.EssayPassPercentage = [];
+// Stores QuestionSet PassPercentage
+ILD.questionSetPassPercentage = [];
 
-  /**
-   * Stores Branching scenario info
-   * @type {Array}
-   */
-  ILD.BranchingScenario = [];
+// Stores Essay PassPercentage
+ILD.EssayPassPercentage = [];
 
-  /**
-   * Stores maxScore of interactions
-   * @type {Int}
-   */
-  ILD.maxScore = 0;
+// Stores Branching scenario info
+ILD.BranchingScenario = [];
 
-  /**
-   * Stores score of interactions
-   * @type {Float}
-   */
-  ILD.score = 0;
+// Stores maxScore of interactions
+ILD.maxScore = 0;
 
-  /**
-   * Stores percentage of interactions progress
-   * @type {Float}
-   */
-  ILD.percentage = 0;
+// Stores score of interactions
+ILD.score = 0;
 
-  /**
-   * Internal H5P function listening for xAPI answered events and stores scores.
-   *
-   * @param {H5P.XAPIEvent} event
-   */
-  ILD.xAPIAnsweredListener = function (event) {
-    var contentId = event.getVerifiedStatementValue([
-      "object",
-      "definition",
-      "extensions",
-      "http://h5p.org/x-api/h5p-local-content-id",
-    ]);
-    var isInteraction = false;
+// Stores percentage of interactions progress
+ILD.percentage = 0;
 
-    if (event.data.statement.object.objectType == "Activity") {
-      isInteraction = true;
-    }
+// Internal H5P function listening for xAPI answered events and stores scores
+ILD.xAPIAnsweredListener = (event) => {
+  const contentId = event.getVerifiedStatementValue([
+    'object',
+    'definition',
+    'extensions',
+    'http://h5p.org/x-api/h5p-local-content-id',
+  ]);
+  let isInteraction = false;
 
-    if (
-      isInteraction &&
-      event.getVerb() === "answered" &&
-      typeof ILD.questionSetPassPercentage[contentId] === "undefined" &&
-      typeof ILD.singleChoiceInteractions[contentId] === "undefined" &&
-      typeof ILD.EssayPassPercentage[contentId] === "undefined" &&
-      typeof ILD.BranchingScenario[contentId] === "undefined"
-    ) {
-      var score = event.getScore();
-      var maxScore = event.getMaxScore();
-      var subContentId = event.data.statement.object.id;
-      subContentId = subContentId.split("subContentId=");
-      subContentId = subContentId[1];
+  if (event.data.statement.object.objectType === 'Activity') {
+    isInteraction = true;
+  }
 
-      if (ILD.subIds.indexOf(subContentId) != -1) {
-        if (typeof ILD.interactions[contentId] === "undefined") {
-          ILD.interactions[contentId] = 1;
-        }
+  if (
+    isInteraction &&
+    event.getVerb() === 'answered' &&
+    typeof ILD.questionSetPassPercentage[contentId] === 'undefined' &&
+    typeof ILD.singleChoiceInteractions[contentId] === 'undefined' &&
+    typeof ILD.EssayPassPercentage[contentId] === 'undefined' &&
+    typeof ILD.BranchingScenario[contentId] === 'undefined'
+  ) {
+    const score = event.getScore();
+    const maxScore = event.getMaxScore();
+    let subContentId = event.data.statement.object.id.split('subContentId=')[1];
 
-        // ILD.score += score;
-        // ILD.maxScore += maxScore;
-        var interactions = ILD.interactions[contentId];
-
-        ILD.percentage =
-          ILD.percentage + (score / maxScore / interactions) * 100;
-
-        ILD.setResult(contentId, ILD.percentage, 100);
-      } else if (
-        ILD.subIds.indexOf(subContentId) == -1 &&
-        ILD.subIds.length == 0
-      ) {
-        var percentage = (score / maxScore) * 100;
-
-        ILD.setResult(contentId, percentage, 100);
+    if (ILD.subIds.indexOf(subContentId) !== -1) {
+      if (typeof ILD.interactions[contentId] === 'undefined') {
+        ILD.interactions[contentId] = 1;
       }
-    }
 
-    // Check if QuestionSet is completed and percentage is set.
-    if (
-      typeof ILD.questionSetPassPercentage[contentId] !== "undefined" &&
-      event.getVerb() === "completed"
-    ) {
-      var score = event.getScore();
-      var maxScore = event.getMaxScore();
-      var percentage = (score / maxScore) * 100;
-      var passPercentage = ILD.questionSetPassPercentage[contentId];
-
-      if (percentage >= passPercentage) {
-        ILD.setResult(contentId, 100, 100);
-      } else {
-        ILD.setResult(contentId, percentage, 100);
-      }
-    }
-
-    // Check if Essay is scored.
-    if (
-      typeof ILD.EssayPassPercentage[contentId] !== "undefined" &&
-      event.getVerb() === "scored"
-    ) {
-      var score = event.getScore();
-      var maxScore = event.getMaxScore();
-      var percentage = (score / maxScore) * 100;
-
+      const interactions = ILD.interactions[contentId];
+      ILD.percentage += (score / maxScore / interactions) * 100;
+      ILD.setResult(contentId, ILD.percentage, 100);
+    } else if (ILD.subIds.indexOf(subContentId) === -1 && ILD.subIds.length === 0) {
+      const percentage = (score / maxScore) * 100;
       ILD.setResult(contentId, percentage, 100);
     }
+  }
 
-    // Check if SingelChoiceSet is completed.
-    if (
-      typeof ILD.singleChoiceInteractions[contentId] !== "undefined" &&
-      event.getVerb() === "completed"
-    ) {
-      var score = event.getScore();
-      var maxScore = event.getMaxScore();
-      var percentage = (score / maxScore) * 100;
+  // Handle QuestionSet completion
+  if (typeof ILD.questionSetPassPercentage[contentId] !== 'undefined' && event.getVerb() === 'completed') {
+    const score = event.getScore();
+    const maxScore = event.getMaxScore();
+    const percentage = (score / maxScore) * 100;
+    const passPercentage = ILD.questionSetPassPercentage[contentId];
 
-      ILD.setResult(contentId, percentage, 100);
-    }
-
-    // Check if BranchingScenario is completed.
-    if (
-      typeof ILD.BranchingScenario[contentId] !== "undefined" &&
-      event.getVerb() === "completed"
-    ) {
+    if (percentage >= passPercentage) {
       ILD.setResult(contentId, 100, 100);
+    } else {
+      ILD.setResult(contentId, percentage, 100);
     }
-  };
+  }
 
-  /**
-   * Post answered results for user and set progress.
-   *
-   * @param {number} contentId
-   *   Identifies the content
-   * @param {number} score
-   *   Achieved score/points
-   * @param {number} maxScore
-   *   The maximum score/points that can be achieved
-   */
-  ILD.setResult = function (contentid, score, maxScore) {
-   
-    var promises = ajax.call([
-      {
-        methodname: "format_mooin4_setgrade",
-        args: { contentid: contentid, score: score, maxscore: maxScore },
-      },
-    ]);    
-  };
+  // Handle Essay score
+  if (typeof ILD.EssayPassPercentage[contentId] !== 'undefined' && event.getVerb() === 'scored') {
+    const score = event.getScore();
+    const maxScore = event.getMaxScore();
+    const percentage = (score / maxScore) * 100;
+    ILD.setResult(contentId, percentage, 100);
+  }
 
-  /**
-   * Count interactions layers from interactive video element.
-   *
-   * @param contentId
-   * @param content
-   */
-  ILD.getVideoInteractions = function (contentId, content) {
-    var interactions = content.interactiveVideo.assets.interactions;
-    var summaries = content.interactiveVideo.summary.task.params.summaries;
-    var notAllowedInteractions = [
-      "H5P.Text",
-      "H5P.Table",
-      "H5P.Link",
-      "H5P.Image",
-      "H5P.GoToQuestion",
-      "H5P.Nil",
-      "H5P.IVHotspot",
-    ];
-    var interactionsCounter = 0;
-    var summariesCounter = 0;
+  // Handle SingleChoiceSet completion
+  if (typeof ILD.singleChoiceInteractions[contentId] !== 'undefined' && event.getVerb() === 'completed') {
+    const score = event.getScore();
+    const maxScore = event.getMaxScore();
+    const percentage = (score / maxScore) * 100;
+    ILD.setResult(contentId, percentage, 100);
+  }
 
-    if (typeof interactions === "object") {
-      $.each(interactions, function (i) {
-        var library = interactions[i].action.library;
-        var subid = interactions[i].action.subContentId;
-        var foundItem = false;
+  // Handle BranchingScenario completion
+  if (typeof ILD.BranchingScenario[contentId] !== 'undefined' && event.getVerb() === 'completed') {
+    ILD.setResult(contentId, 100, 100);
+  }
+};
 
-        $.each(notAllowedInteractions, function (j) {
-          if (library.indexOf(notAllowedInteractions[j]) > -1) {
-            foundItem = true;
-          }
-        });
+// Post answered results for user and set progress
+ILD.setResult = (contentId, score, maxScore) => {
+  window.console.log(score);
+  ILD.reactive.dispatch(
+    "updateSectionprogress",
+    ILD.sectionId,
+    contentId,
+    score,
+    maxScore
+  );
+  // ajax.call([
+  //   {
+  //     methodname: 'format_mooin4_setgrade',
+  //     args: { contentid: contentId, score, maxscore: maxScore },
+  //   },
+  // ]);
+};
 
-        if (!foundItem) {
-          interactionsCounter++;
-          ILD.subIds.push(subid);
-        }
-      });
+// Count interactions layers from interactive video element
+ILD.getVideoInteractions = (contentId, content) => {
+  const interactions = content.interactiveVideo.assets.interactions;
+  const summaries = content.interactiveVideo.summary.task.params.summaries;
+  const notAllowedInteractions = ['H5P.Text', 'H5P.Table', 'H5P.Link', 'H5P.Image', 'H5P.GoToQuestion', 'H5P.Nil', 'H5P.IVHotspot'];
 
-      ILD.interactions[contentId] = interactionsCounter;
-    }
+  let interactionsCounter = 0;
 
-    if (
-      typeof interactions === "undefined" ||
-      (typeof interactions === "object" && interactionsCounter == 0)
-    ) {
-      $(".h5p-iframe")[0].contentWindow.onload = function () {
-        $(".h5p-iframe")[0].contentWindow.H5P.instances[0].video.on(
-          "stateChange",
-          function (event) {
-            if (event.data === 0) {
-              ILD.setResult(contentId, 100, 100);
-            }
-          }
-        );
-      };
-    }
+  if (typeof interactions === 'object') {
+    $.each(interactions, (i) => {
+      const library = interactions[i].action.library;
+      const subid = interactions[i].action.subContentId;
 
-    if (summaries.length) {
-      var summary = false;
-
-      $.each(summaries, function (s) {
-        if (typeof summaries[s].summary !== "undefined") {
-          var subId = content.interactiveVideo.summary.task.subContentId;
-
-          ILD.subIds.push(subId);
-          summary = true;
-        }
-      });
-
-      if (summary) {
-        ILD.interactions[contentId] = interactionsCounter + 1;
+      if (!notAllowedInteractions.some((item) => library.includes(item))) {
+        interactionsCounter++;
+        ILD.subIds.push(subid);
       }
-    }
-  };
-
-  /**
-   * Count interactions layers from SingleChoice element.
-   *
-   * @param contentId
-   * @param content
-   */
-  ILD.getSingleChoiceInteractions = function (contentId, content) {
-    var interactions = content.choices;
-
-    $.each(interactions, function (s) {
-      var subid = interactions[s].subContentId;
-      ILD.subIds.push(subid);
     });
 
-    ILD.singleChoiceInteractions[contentId] = interactions.length;
-  };
+    ILD.interactions[contentId] = interactionsCounter;
+  }
 
-  /**
-   *
-   * @param contentId
-   * @param content
-   */
-  ILD.getQuestionSetPercentage = function (contentId, content) {
-    ILD.questionSetPassPercentage[contentId] = content.passPercentage;
-  };
+  if (!interactions || (typeof interactions === 'object' && interactionsCounter === 0)) {
 
-  /**
-   *
-   * @param contentId
-   * @param content
-   */
-  ILD.getEssayPercentage = function (contentId, content) {
-    ILD.EssayPassPercentage[contentId] = content.behaviour.percentagePassing;
-  };
+//quick fix
+    /*
+    $('.h5p-iframe')[0].contentWindow.onload = () => {
+      $('.h5p-iframe')[0].contentWindow.H5P.instances[0].video.on('stateChange', (event) => {
+        if (event.data === 0) {
+        */
+    ILD.setResult(contentId, 100, 100);
 
-  /**
-   * Check if library is InteractiveVideo or QuestionSet.
-   */
-  ILD.checkLibrary = function () {
-    var contentId = $(".h5p-iframe.h5p-initialized").data("content-id");
-
-    if (typeof contentId !== "undefined") {
-      var contentData = H5PIntegration.contents["cid-" + contentId];
-      var content = JSON.parse(contentData.jsonContent);
-      var library = contentData.library;
-
-      if (library.indexOf("H5P.InteractiveVideo") > -1) {
-        ILD.getVideoInteractions(contentId, content);
-      } else if (library.indexOf("H5P.QuestionSet") > -1) {
-        ILD.getQuestionSetPercentage(contentId, content);
-      } else if (library.indexOf("H5P.SingleChoiceSet") > -1) {
-        ILD.getSingleChoiceInteractions(contentId, content);
-      } else if (library.indexOf("H5P.Essay") > -1) {
-        ILD.getEssayPercentage(contentId, content);
-      } else if (library.indexOf("H5P.BranchingScenario") > -1) {
-        ILD.BranchingScenario[contentId] = 1;
-      }
-    }
-  };
-
-  return {
-    init: function (H5PInstance) {
-      ILD.checkLibrary();
-      H5PInstance.externalDispatcher.on("xAPI", ILD.xAPIAnsweredListener);
-    },
-  };
+    //quick fix
+    /*
+  }
 });
+};
+*/
+  }
+
+  if (summaries.length) {
+    let summary = false;
+
+    $.each(summaries, (s) => {
+      if (typeof summaries[s].summary !== 'undefined') {
+        const subId = content.interactiveVideo.summary.task.subContentId;
+        ILD.subIds.push(subId);
+        summary = true;
+      }
+    });
+
+    if (summary) {
+      ILD.interactions[contentId] = interactionsCounter + 1;
+    }
+  }
+};
+
+// Count interactions layers from SingleChoice element
+ILD.getSingleChoiceInteractions = (contentId, content) => {
+  const interactions = content.choices;
+
+  $.each(interactions, (s) => {
+    const subid = interactions[s].subContentId;
+    ILD.subIds.push(subid);
+  });
+
+  ILD.singleChoiceInteractions[contentId] = interactions.length;
+};
+
+// Get QuestionSet PassPercentage
+ILD.getQuestionSetPercentage = (contentId, content) => {
+  ILD.questionSetPassPercentage[contentId] = content.passPercentage;
+};
+
+// Get Essay PassPercentage
+ILD.getEssayPercentage = (contentId, content) => {
+  ILD.EssayPassPercentage[contentId] = content.behaviour.percentagePassing;
+};
+
+// Check if library is InteractiveVideo or QuestionSet
+ILD.checkLibrary = (H5PIntegration, H5PInstance) => {
+
+  window.console.log(H5PInstance);
+  const contentId = H5PInstance.contentId;
+
+
+  if (typeof contentId !== 'undefined') {
+    const contentData = H5PIntegration.contents[`cid-${contentId}`];
+    const content = JSON.parse(contentData.jsonContent);
+    const library = contentData.library;
+
+
+    if (library.includes('H5P.InteractiveVideo')) {
+      ILD.getVideoInteractions(contentId, content);
+    } else if (library.includes('H5P.QuestionSet')) {
+      ILD.getQuestionSetPercentage(contentId, content);
+    } else if (library.includes('H5P.SingleChoiceSet')) {
+      ILD.getSingleChoiceInteractions(contentId, content);
+    } else if (library.includes('H5P.Essay')) {
+      ILD.getEssayPercentage(contentId, content);
+    } else if (library.includes('H5P.BranchingScenario')) {
+      ILD.BranchingScenario[contentId] = 1;
+    }
+  }
+};
+
+
+export default {
+  init(H5PInstance, H5PIntegration, sectionId, reactive) {
+    ILD.sectionId = sectionId;
+    ILD.reactive = reactive;
+    ILD.checkLibrary(H5PIntegration, H5PInstance.instances[0]);
+    H5PInstance.externalDispatcher.on('xAPI', ILD.xAPIAnsweredListener);
+  },
+};
