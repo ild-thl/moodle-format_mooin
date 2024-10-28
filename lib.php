1067ae125dbed5bd8eb88801c8775d410d19fa7b
tinjohn <tina.john@th-luebeck.de>
2024-10-28 22:10:08 +0100

    fix M41-169 more save

--- lib.php
+++ lib.php
@@ -668,8 +668,8 @@ class format_mooin4 extends core_courseformat\base {
         if ($course = $DB->get_record('course', array('id' => $this->courseid))) {
             // $course->enablecompletion = 1;
             // $course->showcompletionconditions = 0;
-            // $course->newsitems = 1;
-            // $DB->update_record('course', $course);
+            $course->newsitems = 1;
+            $DB->update_record('course', $course);
         }
 
         return $this->update_format_options($data);
