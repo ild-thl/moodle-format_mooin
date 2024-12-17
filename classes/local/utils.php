11d8ff069914438a1087bbe90982b741aa84a497
tinjohn <tina.john@th-luebeck.de>
2024-12-17 01:33:33 +0100

    single user fixed: in course participant list incorrectly addressed in the plural

--- classes/local/utils.php
+++ classes/local/utils.php
@@ -283,11 +283,12 @@ class utils {
 
         $context = context_course::instance($courseid);
         $has_capability = has_capability('moodle/course:viewparticipants', $context);
-
+        $singleuser = $user_count == 1 ? true : false;
         $templatecontext = [
             'user_count' => $user_count,
             'user_list' => $user_list,
             'has_capability' => $has_capability,
+            'singleuser' => $singleuser,
         ];
 
         return $templatecontext;
