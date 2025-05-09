567bc7739821a67c85364d24e4268844225db6d0
Tom <thomas.muschal@th-luebeck.de>
2025-05-09 11:22:40 +0200

    Update lang string

--- lang/en/format_mooin4.php
+++ lang/en/format_mooin4.php
@@ -197,7 +197,7 @@ $string['youareeditingsection'] = 'Warning: You are editing Section 0!';
 $string['mycoursecompetencies'] = 'My course competencies';
 
 // course side bar
-$string['include_in_sidebar'] = 'Display links in course index';
+$string['include_in_sidebar'] = 'Display links in course index (only available when globally activated)';
 $string['global_tile_visibility'] = 'Display mooin tiles and links in course index in course globally';
 $string['toggle_global_badge_visibility'] = 'Toggle global badge tile and course index link visibility';
 $string['toggle_global_certificate_visibility'] = 'Toggle global certificate tile and course index link visibility';
