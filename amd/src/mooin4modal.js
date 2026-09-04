cb3b845f1d5c9ce2e7c5568cefa728caa3a07e2f
Alexander Müller <alexander.mueller@oncampus.de>
2026-09-04 09:42:01 +0200

    fix: remove deprecated ModalRegistry

--- amd/src/mooin4modal.js
+++ amd/src/mooin4modal.js
@@ -1,12 +1,6 @@
 import Modal from 'core/modal';
-import ModalRegistry from 'core/modal_registry';
 
 export default class Mooin4Modal extends Modal {
     static TYPE = "format_mooin4/mooin4Modal";
     static TEMPLATE = "format_mooin4/local/content/modals/mooin4Modal";
-}
-let registered = false;
-if (!registered) {
-    ModalRegistry.register(Mooin4Modal.TYPE, Mooin4Modal, Mooin4Modal.TEMPLATE);
-    registered = true;
 }
